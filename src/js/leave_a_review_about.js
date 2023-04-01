00fe7cc62b2016f212237cf2e64973a70631b744
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-04-01 22:32:27 +0300

    telef modal

--- src/js/leave_a_review_about.js
+++ src/js/leave_a_review_about.js
@@ -4,6 +4,7 @@
     closeModalBtn: document.querySelector('[leave_a_review_about-close]'),
     modal_reviews: document.querySelector('[leave_a_review_about-modal]'),
   };
+  console.log('open');
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
@@ -12,3 +13,19 @@
     refs.modal_reviews.classList.toggle('is-hidden');
   }
 })();
+
+// export const fun = () => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[leave_a_review_about-open]'),
+//     closeModalBtn: document.querySelector('[leave_a_review_about-close]'),
+//     modal_reviews: document.querySelector('[leave_a_review_about-modal]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     console.log('OPEN');
+//     refs.modal_reviews.classList.toggle('is-hidden');
+//   }
+// };
