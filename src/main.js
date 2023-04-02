5bc3c2418ba422eaff4cbda0fce6992dad4a0c34
ilkash <126604951+ilkash@users.noreply.github.com>
2023-04-02 15:39:59 +0300

    слайдер

--- src/main.js
+++ src/main.js
@@ -4,3 +4,5 @@ import './js/modal_explore_our_new.js';
 import './js/mobile-menu.js';
 import './js/leave_a_review_about.js';
 import './js/video.js';
+import './js/slick.min.js';
+import './js/script.js';
