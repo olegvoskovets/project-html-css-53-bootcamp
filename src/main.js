c4f739cbfe13677e6959ec1198c96b748762d61f
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-04-02 13:07:14 +0300

    cmmit main

--- src/main.js
+++ src/main.js
@@ -1,14 +1,6 @@
-<<<<<<< Updated upstream
 import './js/swiper.js';
 import './js/modal_explore_our_new.js';
 // import './js/buy_now.js';
 import './js/mobile-menu.js';
 import './js/leave_a_review_about.js';
 import './js/video.js';
-=======
-//import './js/swiper.js';
-// import './js/modal_explore_our_new.js';
-// import './js/buy_now.js';
-// import './js/leave_a_review.js';
-// export const express = require('./js/leave_a_review.js');
->>>>>>> Stashed changes
