6a042957242a7ead6cd882a9ce53a1c10404097e
ginkosoul <ginkosoul@gmail.com>
2023-04-03 09:09:19 +0300

    Added java script for buttons

--- src/js/order.js
+++ src/js/order.js
@@ -1,16 +1,21 @@
 const MODAL_ACTIVE_CLASS_NAME = 'modal-active';
 
-const formModal = document.querySelector('#form-modal');
-const successModal = document.querySelector('#success-modal');
-const form = document.querySelector('#form');
+const formModal = document.querySelector('#modal-order');
+const successModal = document.querySelector('#success-modal-order');
+const form = document.querySelector('#form-order');
 
-const openFormModalBtn = document.querySelector('#open-form-modal-btn');
-const launchBtn = document.querySelector('#launch-btn');
-const closeBtns = document.querySelectorAll('.close-btn');
+const openFormModalBtn = document.querySelectorAll('.open-order-modal-btn');
+const launchBtn = document.querySelector('#order-submit-btn');
+const closeBtns = document.querySelectorAll('.close-btn-order');
 
-openFormModalBtn.addEventListener('click', () => {
-    formModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
+openFormModalBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        formModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
+    })
 })
+// openFormModalBtn.addEventListener('click', () => {
+//     formModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
+// })
 
 const closeFormModal = () => {
     formModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
@@ -39,21 +44,6 @@ function clearFormFields() {
         field.value = ''
     });
 }
-
-function showGooseAnim() {
-    const targetContainer = document.querySelector('.modal-form');
-    const gusImage = document.createElement('img');
-    gusImage.setAttribute('src', './img/gus-anim.gif');
-    gusImage.classList.add('gus-anim');
-
-    targetContainer.appendChild(gusImage);
-
-    setTimeout(2000, () => {
-        targetContainer.removeChild(gusImage);
-    })
-}
-
-
 form.addEventListener('submit', e => {
     e.preventDefault();
     const formData = new FormData(form);
@@ -64,15 +54,12 @@ form.addEventListener('submit', e => {
       body: new URLSearchParams(formData).toString(),
     })
       .then(() => {
-        showGooseAnim();
-
         setTimeout(() => {
             closeFormModal();
-            setTimeout(openSuccessModal, 700);
-            setTimeout(closeSuccessModal, 4000);
+            setTimeout(openSuccessModal, 250);
+            setTimeout(closeSuccessModal, 6000);
             clearFormFields();
-        }, 4000);
+        }, 250);
       })
       .catch((error) => console.log('Sending form failed'));
 })
-
